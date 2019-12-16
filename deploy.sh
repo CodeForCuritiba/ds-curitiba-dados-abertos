2eda4115402ca397daba9c940108e9d8549b5c71
Jonhnatha Trigueiro <joepreludian@gmail.com>
2019-12-16 00:14:53 -0300

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+figlet "Deploy 156";
+
+mkdir _deploy;
+cp -Rv *.ipynb _deploy;
+
+pip install requests;
+
+figlet 'Test Curitiba156';
+pytest --cov=curitiba156;
+
+cd _deploy;
+
+jupyter nbconvert --to script *.ipynb;
+
+for jupyter_script in *.py
+do 
+  echo "Processing ${jupyter_script}...";
+  mv "${jupyter_script}" "../jupyter_generated_${jupyter_script}";
+done
+
+cd ..;
+
+figlet "Running notebooks";
+for jupyter_script in jupyter_generated_*.py
+do 
+  echo " * Running ${jupyter_script}...";
+  python3 "${jupyter_script}";
+done
+
+figlet "Compressing";
+cd clean_data;
+
+dnf install -y bzip2;
+bzip2 --best -v *;
+
+cd ..;
+
+figlet "Clean";
+rm -f jupyter_generated_*;
+rm -Rf _deploy;
+
+echo "Processing Finished!";
