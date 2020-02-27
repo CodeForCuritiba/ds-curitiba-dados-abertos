dfb37271a2f9ea700b30c40ffe488a409926b12f
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-02-27 00:13:29 -0300

    UPdate start script

--- start.sh
+++ start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-figlet "CODE4CWB"
-echo "Starting Curitiba Dados abertos!";
+figlet "CODE FOR CURITIBA"
+echo "Iniciando painel dados abertos";
 
 # Instalando bibliotecas
-pip install --upgrade requests pandarallel jupyterthemes;
+figlet "Pacotes Python..."
+pip install --upgrade pip;
+pip install --upgrade requests pandarallel jupyterthemes curitiba-dados-abertos;
 
-# Instalando libs extras via PIP
-pip install git+https://github.com/kavgan/word_cloud.git
-
-figlet "Jupyter Start";
+figlet "Pacotes Jupyter...";
 
 # Ativando extensões extras do jupyter
 jupyter nbextension enable --py --sys-prefix qgrid;
 jupyter nbextension enable --py --sys-prefix widgetsnbextension;
 
-figlet "Jupyter Notebook";
-
+figlet "Iniciando Jupyter";
+echo "Carregando serviço na porta 8888"
 # Customizando Jupyter Notebook - https://github.com/dunovank/jupyter-themes
 jt -fs 95 -altp -tfs 11 -nfs 115 -cellw 94% -T
 
